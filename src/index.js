4f2bf3893525dee0d6b2db1dbcdc03f636cb9211
ironylait <ironylait@mail.ru>
2019-03-20 19:33:25 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var counter = 0;
+  var triangles = 0;
+
+    for (var i = 0; i < preferences.length; i++) { 
+      
+      var firstSpichonee  = preferences[i];
+      var secondSpichonee = preferences[firstSpichonee - 1];
+      var thirdSpichonee  = preferences[secondSpichonee - 1]; 
+
+      if (thirdSpichonee === i + 1) {
+        counter++;
+      }
+    }
+
+ triangles =(Math.floor(counter / 3)) ;
+ return triangles; 
 };
+
+
